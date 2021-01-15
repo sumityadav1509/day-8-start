25a18adbdc3a01ad926580fbcb0fc1406ebf982a
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-15 23:58:30 +0000

    Positional Arguments.

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ def greet_with(name,location):
   print(f"Hello {name}")
   print(f"What is it like in  {location}")
 
-greet_with(input("Name: "), input("Location: "))
+greet_with(input("Name: "), input("Location: ")) 
