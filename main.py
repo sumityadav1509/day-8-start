6b8515efbac0c101d23120ea5eefc76b76e9cc1f
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-16 20:18:42 +0000

    Used math module to round-off the result.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+
+
+import math
+
 def paint_area(height,width,per_can_area):
   number_of_cans=(int(height)*int(width))/int(per_can_area) 
-  print(round(number_of_cans))
+  print(math.ceil(number_of_cans))
 
 
 paint_area(input("Height: "), input("Width: ") , input("Per Can: "))
